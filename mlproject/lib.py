605cdf89bdbc8496d59cebb40fe1a7e452e46f78
Danny Cardoso <dcardoso066@gmail.com>
2021-11-15 11:37:05 +0100

    Initial Push

--- mlproject/lib.py
+++ mlproject/lib.py
@@ -1,2 +1,5 @@
 def hello_world():
     return "Hello world from mlproject"
+
+def try_me():
+    return print("Hello it's me, no it's you, heuuu ?")
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,4 +1,8 @@
 from mlproject.lib import hello_world
+from mlproject.lib import try_me
 
 def test_length_of_hello_world():
     assert len(hello_world()) != 0
+    
+def test_try_me():
+    assert len(try_me()) != 0
